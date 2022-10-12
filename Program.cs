d44f167a3ae921babc821312e4bdd11e98e01c41
JacobFrye091 <vasilescuandrei945@gmail.com>
2022-10-12 16:26:36 +0300

    List<cardHolder>()

--- Program.cs
+++ Program.cs
@@ -55,4 +55,42 @@ public class cardHolder
     {
         balance = newBalance;
     }
+    public static void Main(String[] args)
+    {
+        void printOptions()
+        {
+            Console.WriteLine("Please choose from one of the following options...");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Deposit");
+            Console.WriteLine("3. Deposit");
+            Console.WriteLine("4. Deposit");
+        }
+        void deposit(cardHolder currentUser)
+        {
+            Console.WriteLine("How much $$ would you like to deposit? ");
+            double deposit = Double.Parse(Console.ReadLine());
+            currentUser.setBalance(deposit);
+            Console.WriteLine($"Thank you for your $$. Your new balance is: {currentUser.getBalance()}");
+        }
+        void withdraw(cardHolder currentUser)
+        {
+            Console.WriteLine("How much $$ would you like to withdraw: ");
+            double withdraw = Double.Parse(Console.ReadLine());
+            //Check if the user has enough money
+            if (currentUser.getBalance() > withdraw)
+            {
+                Console.WriteLine("Insufficient balance ;(");
+            }
+            else
+            {
+                currentUser.setBalance(currentUser.getBalance() - withdraw);
+                Console.WriteLine("You are good to go. Thank you ;)");
+            }
+        }
+        void balance(cardHolder currentUser)
+        {
+            Console.WriteLine("Current balance: "+currentUser.getBalance());
+        }
+        List<cardHolder> cardHolders = new List<cardHolder>();
+    }
 }
