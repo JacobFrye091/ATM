bb3226176280bdc6c232c7d6e566e1b62e63faf1
JacobFrye091 <vasilescuandrei945@gmail.com>
2022-10-12 16:43:36 +0300

    printOptions()

--- Program.cs
+++ Program.cs
@@ -120,5 +120,55 @@ public class cardHolder
                 Console.WriteLine("Card not recognized. Please try again");
             }
         }
+        Console.WriteLine("Please enter your pin: ");
+        int userPin = 0;
+        while (true)
+        {
+            try
+            {
+                userPin = int.Parse(Console.ReadLine());
+                if (currentUser.getPin() == userPin)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect pin. Please try again.");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Incorrect pin. Please try again.");
+            }
+        }
+        Console.WriteLine($"Welcome {currentUser.getFirstName()}");
+        int option = 0;
+        do
+        {
+            printOptions();
+            try
+            {
+                option = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+
+            }
+            if (option == 1)
+            {
+                deposit(currentUser);
+            }else if (option == 2)
+            {
+                withdraw(currentUser);
+            }else if (option == 3)
+            {
+                balance(currentUser);
+            }
+            else
+            {
+                option = 0;
+            }
+        } while (option != 4);
+        Console.WriteLine("Thank you! Have a nice day");
     }
 }
